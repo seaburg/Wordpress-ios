cc71a73ace813b9294c2208333ec094b0a807632
Evgeniy Yurtaev <evgeniyyury@gmail.com>
2014-12-31 16:14:34 +0300

    Create abstract class for requests

---
+++ Wordpress/Network/Core/WPRequest.h
@@ -0,0 +1,21 @@
+//
+//  WPRequest.h
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 31/12/14.
+//  Copyright (c) 2014 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface WPRequest : MTLModel<MTLJSONSerializing>
+
+@property (strong, nonatomic) NSArray *routeObjects;
+
++ (NSString *)path;
+
++ (NSString *)method;
+
++ (Class)responseClass;
+
+@end
---
+++ Wordpress/Network/Core/WPRequest.m
@@ -0,0 +1,38 @@
+//
+//  WPRequest.m
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 31/12/14.
+//  Copyright (c) 2014 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import "WPRequest.h"
+
+@implementation WPRequest
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey
+{
+    return @{
+        @keypath([WPRequest new], routeObjects): [NSNull null],
+    };
+}
+
++ (NSString *)path
+{
+    NSAssert(NO, @"should be overridden in subclasses");
+    return nil;
+}
+
++ (NSString *)method
+{
+    NSAssert(NO, @"should be overridden in subclasses");
+    return nil;
+}
+
++ (Class)responseClass
+{
+    NSAssert(NO, @"should be overridden in subclasses");
+    return nil;
+}
+
+@end
