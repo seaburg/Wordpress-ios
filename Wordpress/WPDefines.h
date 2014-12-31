3219361e10fdef901f5c8716f4db92a68d10e4cd
Evgeniy Yurtaev <evgeniyyury@gmail.com>
2014-12-31 23:49:38 +0300

    Add api error domain

---
+++ Wordpress/WPDefines.h
@@ -0,0 +1,12 @@
+//
+//  WPDefines.h
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 31/12/14.
+//  Copyright (c) 2014 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// error domains
+extern NSString *const WPAPIErrorDomain;
---
+++ Wordpress/WPDefines.m
@@ -0,0 +1,11 @@
+//
+//  WPDefines.m
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 31/12/14.
+//  Copyright (c) 2014 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import "WPDefines.h"
+
+NSString *const WPAPIErrorDomain = @"com.wordpress.error.api";
