fbdfb038ad18401e722634089c6299ab7593ecd0
seaburg <evgeniyyurt@gmail.com>
2015-01-10 20:58:00 +0300

    Create the post view model

---
+++ Wordpress/ViewModels/WPPostViewModel.h
@@ -0,0 +1,26 @@
+//
+//  WPPostViewModel.h
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 09/01/15.
+//  Copyright (c) 2015 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import "WPViewModel.h"
+
+@class RACSignal;
+
+@interface WPPostViewModel : WPViewModel
+
+@property (copy, nonatomic, readonly) NSString *HTMLString;
+
+@property (strong, nonatomic, readonly) NSURL *baseURL;
+
+@property (assign, nonatomic, readonly) NSInteger numberOfComments;
+
+- (instancetype)initWithSiteID:(NSNumber *)siteID postID:(NSNumber *)postID;
+
+// reloadData : -> RACSignal _
+- (RACSignal *)reloadData;
+
+@end
---
+++ Wordpress/ViewModels/WPPostViewModel.m
@@ -0,0 +1,100 @@
+//
+//  WPPostViewModel.m
+//  Wordpress
+//
+//  Created by Evgeniy Yurtaev on 09/01/15.
+//  Copyright (c) 2015 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+#import "WPPostViewModel.h"
+#import "WPClient.h"
+#import "WPGetPostRequest.h"
+#import "WPPostOfSiteRoute.h"
+#import "WPPost.h"
+#import "WPUser.h"
+
+#import "WPViewModel+Friend.h"
+
+static NSString *const WPHTMLStringOfPostFormat = @"<html><head><style>*{max-width: 100%%};</style></head><body><h1>%@</h1><h5>%@</h5>%@<script>elements = document.getElementsByTagName('*')for(i = 0;i < elements.length; i++) {elements[i].removeAttribute('height')}</script></body></html>";
+
+@interface WPPostViewModel ()
+
+@property (strong, nonatomic) WPPost *post;
+
+@property (strong, nonatomic) NSNumber *siteID;
+@property (strong, nonatomic) NSNumber *postID;
+
+@property (copy, nonatomic) NSString *HTMLString;
+@property (strong, nonatomic) NSURL *baseURL;
+@property (assign, nonatomic) NSInteger numberOfComments;
+
+@end
+
+@implementation WPPostViewModel
+
+- (instancetype)initWithSiteID:(NSNumber *)siteID postID:(NSNumber *)postID
+{
+    NSParameterAssert(siteID);
+    NSParameterAssert(postID);
+    
+    self = [super init];
+    if (self) {
+        self.siteID = siteID;
+        self.postID = postID;
+        
+        RAC(self, HTMLString) = [RACObserve(self, post)
+            map:^id(WPPost *post) {
+                NSString *HTMLString = [NSString stringWithFormat:WPHTMLStringOfPostFormat, post.title, post.author.name, post.content];
+                return HTMLString;
+            }];
+        
+        RAC(self, numberOfComments, @0) = RACObserve(self, post.numberOfComments);
+        RAC(self, baseURL) = RACObserve(self, post.URL);
+    }
+    return self;
+}
+
+- (RACSignal *)reloadData
+{
+    return [[[RACSignal
+        createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+            
+            WPPostOfSiteRoute *routeObject = [[WPPostOfSiteRoute alloc] init];
+            routeObject.siteID = self.siteID;
+            routeObject.postID = self.postID;
+            
+            WPGetPostRequest *request = [[WPGetPostRequest alloc] init];
+            request.fields = @[ @"ID", @"site_ID", @"author", @"comment_count", @"content", @"URL" ];
+            request.routeObject = routeObject;
+            
+            RACCompoundDisposable *disposable = [RACCompoundDisposable compoundDisposable];
+            RACMulticastConnection *requestConnection = [[[WPClient sharedInstance] performRequest:request]
+                publish];
+            
+            [disposable addDisposable:[[requestConnection.signal
+                catchTo:[RACSignal empty]]
+                setKeyPath:@keypath([WPPostViewModel new], post) onObject:self]];
+            
+            [disposable addDisposable:[requestConnection.signal subscribe:subscriber]];
+            [disposable addDisposable:[requestConnection connect]];
+            
+            return disposable;
+        }]
+        ignoreValues]
+        catch:^RACSignal *(NSError *error) {
+            RACSignal *signal;
+            if (self.closeSignal) {
+                signal = [self.closeSignal
+                    then:^RACSignal *{
+                        return [RACSignal error:error];
+                    }];
+            } else {
+                signal = [RACSignal error:error];
+            }
+            return signal;
+        }];
+}
+
+@end
