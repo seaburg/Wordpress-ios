2b7ff0be5d3816eab6a9c0cb71eb4a6cf5676f8c
seaburg <evgeniyyurt@gmail.com>
2015-01-11 13:17:36 +0300

    Create the base implementation of preparing for use

--- Wordpress/ViewModels/WPViewModel+Friend.m
+++ Wordpress/ViewModels/WPViewModel+Friend.m
@@ -6,10 +6,17 @@
 //  Copyright (c) 2014 Evgeniy Yurtaev. All rights reserved.
 //
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
 #import "WPViewModel+Friend.h"
 
 @implementation WPViewModel (Friend)
 
 @dynamic closeSignal;
+
+- (RACSignal *)prepareForUse
+{
+    return [RACSignal empty];
+}
 
 @end
